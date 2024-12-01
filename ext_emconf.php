0eb50977dc7217301429f62a04a862453464a5d3
Sybille Peters <sypets@gmx.de>
2024-12-01 14:44:03 +0100

    Add support for v14

--- ext_emconf.php
+++ ext_emconf.php
@@ -15,7 +15,7 @@ $EM_CONF[$_EXTKEY] = array (
   array (
     'depends' =>
     array (
-      'typo3' => '11.5.30-13.9.99',
+      'typo3' => '11.5.30-14.9.99',
     ),
     'suggests' =>
     array (
